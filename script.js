45985843cd0ec3738cd5f3eb1e7cda783e8113f8
Luis Ramos <50378468+luisramos-dev@users.noreply.github.com>
2024-04-09 20:34:34 -0300

    Portfolio Upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Typing Animation */
+var typed = new Typed(".typing",{
+    strings:["","Artista 3D/2D","Designer UX/UI","Desenvolvedor de Jogos"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
